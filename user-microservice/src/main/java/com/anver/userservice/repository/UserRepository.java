d6922a12451b8e2ff83c5db120557164b64c50e1
anver-dev <karinaagp26@gmail.com>
2021-11-18 16:21:09 -0600

    User repository

---
+++ user-microservice/src/main/java/com/anver/userservice/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.anver.userservice.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.anver.userservice.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	public Optional<User> findByUsername(String username);
+	
+	@Query("SELECT u.name FROM user u WHERE u.id in (:pIdList)")
+	public List<String> findByIdList(@Param("pIdList") List<Long> idList);
+	
+}
